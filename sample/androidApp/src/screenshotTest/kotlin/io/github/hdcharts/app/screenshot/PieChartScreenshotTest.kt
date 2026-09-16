a4142bce1dae3e8ec52b79205b66d78a537152e5
hdcode.dev <215285761+hdcodedev@users.noreply.github.com>
2026-09-16 10:52:19 +0200

    fix ktlint

--- sample/androidApp/src/screenshotTest/kotlin/io/github/hdcharts/app/screenshot/PieChartScreenshotTest.kt
+++ sample/androidApp/src/screenshotTest/kotlin/io/github/hdcharts/app/screenshot/PieChartScreenshotTest.kt
@@ -3,8 +3,8 @@ package io.github.hdcharts.app.screenshot
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.ui.Alignment
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.android.tools.screenshot.PreviewTest
